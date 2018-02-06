cbfdcc1608fa443b4632f11369305c1ec3a2f048
alvao cardenas <bdp_rs@hotmail.com>
2018-02-06 18:39:10 -0500

    Terminado

---
+++ Hello/Hello.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+
+
+namespace Hello
+{
+    /// <summary>
+    /// Summary description for Hello
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Hello : System.Web.Services.WebService
+    {
+
+      
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string getJSON()
+        {
+            ClassHello[] saludo = new ClassHello[]
+            {
+            new ClassHello()
+            {
+                Nombre = "Juan"
+            }
+
+            };
+            return new JavaScriptSerializer().Serialize(saludo);
+        }
+
+    }
+}
